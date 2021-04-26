77a31506e47d87550ac98156c7c91454976b33a4
Rolandorosay <invstapp@gmail.com>
2021-04-26 17:36:43 -0400

    update js

--- pacmen.js
+++ pacmen.js
@@ -1,4 +1,5 @@
 var pos = 0;
+
 const pacArray = [
     ['PacMan1.png', 'PacMan2.png'],
     ['PacMan3.png', 'PacMan4.png']
@@ -12,44 +13,40 @@ function setToRandom(scale) {
         x: Math.random() * scale,
         y: Math.random() * scale,
     };
-}
+};
     // Factory to make PacMan at a random position with random velocity
 function makePac() {
-    //
+
     // Returns an object with random values scaled {x: 33, y: 21}
-    //
     let velocity = setToRandom(10); // {x:?, y:?}
     let position = setToRandom(200);
-    //
+    
     // Add image to div id = game
-    //
     let game = document.getElementById('game');
     let newimg = document.createElement('img');
+
     newimg.style.position = 'absolute';
     newimg.src = 'PacMan1.png';
     newimg.width = 100;
-    //
+    
     // Position set below
-    //
     newimg.style.left = position.x;
     newimg.style.top = position.y;
-    //
+    
     // Add new Child image to game
-    //
     game.appendChild(newimg);
-    //
+    
     // Return details in an object
     return {
         position,
         velocity,
         newimg
     };
-}
+};
 
 function update() {
-    //
+    
     // Loop over PacMen array and move each one and move image in DOM
-    //
     pacMen.forEach((item) => {
         checkCollisions(item)
         item.position.x += item.velocity.x;
@@ -57,14 +54,13 @@ function update() {
 
         item.newimg.style.left = item.position.x;
         item.newimg.style.top = item.position.y;
-    });
+    })
     setTimeout(update, 20);
 }
 
 function checkCollisions(item) {
-    //
-    // detect collision with all walls and make pacman bounce
-    //
+    
+    // detect collision with all walls and make PacMan bounce
     if (item.position.x + item.velocity.x + item.newimg.width > window.innerWidth ||
         item.position.x < 0) {
             item.velocity.x = -item.velocity.x;
@@ -79,4 +75,4 @@ function checkCollisions(item) {
 // Will add a new PacMan
 function makeOne() {
     pacMen.push(makePac()); 
-}
+};
